db2e50de9f309a6e7be403249b2d1d7b41c78370
CrioUser <crio-user@criodo.com>
2024-04-03 15:50:13 +0530

    ME_QEATS_V2_MODULE_MOCKITO

--- qeatsbackend/src/main/java/com/crio/qeats/services/RestaurantServiceImpl.java
+++ qeatsbackend/src/main/java/com/crio/qeats/services/RestaurantServiceImpl.java
@@ -52,7 +52,7 @@ public class RestaurantServiceImpl implements RestaurantService {
       currentTime, normalHoursServingRadiusInKms);
     }
     GetRestaurantsResponse response = new GetRestaurantsResponse(restaurant);
-    log.info(response);
+    //log.info(response);
     return response;
   }
 }
